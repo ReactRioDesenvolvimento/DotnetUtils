fb7d0feeb8e64e453c8dd0c0dd0e30e1b44233ac
Gabriel David <gabrielcandez@gmail.com>
2021-12-07 15:42:10 -0300

    Add post to HTTP

--- DotnetUtils/Http.cs
+++ DotnetUtils/Http.cs
@@ -1,9 +1,42 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace ReactRio.Utils;
 
 public static class Http
 {
+    public static async Task<TResponse?> Post<TResponse, TRequest>
+    (
+        string url, TRequest request, JsonSerializerOptions? options = null,
+        int timeout = 0,
+        CancellationToken cancellationToken = default
+    ) where TResponse : class
+    {
+        using var httpClient = new HttpClient();
+        try
+        {
+            if (timeout > 0)
+                httpClient.Timeout = TimeSpan.FromSeconds(timeout);
+
+            using var response = await httpClient.PostAsJsonAsync(url, request, options, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsStringAsync(cancellationToken);
+
+                throw new Exception($"HTTP GET faiure: {error}");
+            }
+
+            var content = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+            return await JsonSerializer.DeserializeAsync<TResponse>(content, options, cancellationToken);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
     public static async Task<TResponse?> Get<TResponse>
     (
         string url, JsonSerializerOptions? options = null, int timeout = 0,
